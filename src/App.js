dcda12e31cc2d44920b69d9e067882c83f207703
Rahied Ishaak <rahied.ishaak.code@gmail.com>
2019-11-07 23:39:28 +0100

    adds basic styling to chart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { VictoryChart, VictoryBar, VictoryTooltip, VictoryGroup } from 'victory';
+import { VictoryChart, VictoryBar, VictoryTooltip, VictoryGroup, VictoryLabel } from 'victory';
 
 class App extends Component {
   state = {
@@ -144,7 +144,8 @@ class App extends Component {
       return {
         x: rating.exerciseID,
         y: rating.rating,
-        label: `${rating.exerciseName} | ${rating.rating}`
+        label: `${rating.exerciseName}
+        Moeilijkheid: ${rating.rating}`
       };
     });
 
@@ -153,420 +154,60 @@ class App extends Component {
       return {
         x: rating.exerciseID,
         y: rating.rating,
-        label: `${rating.exerciseName} | ${rating.rating}`
+        label: `${rating.exerciseName}
+        Vermaak: ${rating.rating}`
       };
     });
 
     const wincTheme = {
-      area: {
-        style: {
-          data: {
-            fill: "#212121"
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 50
-      },
       axis: {
         style: {
-          axis: {
-            fill: "transparent",
-            stroke: "#90A4AE",
-            strokeWidth: 2,
-            strokeLinecap: "round",
-            strokeLinejoin: "round"
-          },
-          axisLabel: {
-            textAnchor: "middle",
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          grid: {
-            fill: "none",
-            stroke: "none"
-          },
-          ticks: {
-            fill: "transparent",
-            size: 5,
-            stroke: "#90A4AE",
-            strokeWidth: 1,
-            strokeLinecap: "round",
-            strokeLinejoin: "round"
-          },
-          tickLabels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 50
+          axis: { stroke: "#4a90e2", strokeWidth: 2 },
+          grid: { stroke: "none" },
+          ticks: { size: 5, stroke: "#4a90e2", strokeWidth: 3 },
+          tickLabels: { padding: 3, fontSize: 10, fill: "#4a90e2" }
+        }
       },
       bar: {
         style: {
-          data: {
-            fill: "#4a90e2",
-            padding: 0,
-            strokeWidth: 0
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 8,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 20
-      },
-      boxplot: {
-        style: {
-          max: {
-            padding: 8,
-            stroke: "#455A64",
-            strokeWidth: 1
-          },
-          maxLabels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          median: {
-            padding: 8,
-            stroke: "#455A64",
-            strokeWidth: 1
-          },
-          medianLabels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          min: {
-            padding: 8,
-            stroke: "#455A64",
-            strokeWidth: 1
-          },
-          minLabels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          q1: {
-            padding: 8,
-            fill: "#455A64"
-          },
-          q1Labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          q3: {
-            padding: 8,
-            fill: "#455A64"
-          },
-          q3Labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        boxWidth: 20,
-        width: 350,
-        height: 350,
-        padding: 50
-      },
-      candlestick: {
-        style: {
-          data: {
-            stroke: "#455A64"
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        candleColors: {
-          positive: "#ffffff",
-          negative: "#455A64"
-        },
-        width: 350,
-        height: 350,
-        padding: 50
-      },
-      chart: {
-        width: 800,
-        height: 300,
-        padding: 50
-      },
-      errorbar: {
-        borderWidth: 8,
-        style: {
-          data: {
-            fill: "transparent",
-            opacity: 1,
-            stroke: "#455A64",
-            strokeWidth: 2
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 50
-      },
-      group: {
-        colorScale: [
-          "#F4511E",
-          "#FFF59D",
-          "#DCE775",
-          "#8BC34A",
-          "#00796B",
-          "#006064"
-        ],
-        width: 350,
-        height: 350,
-        padding: 50
-      },
-      legend: {
-        colorScale: [
-          "#F4511E",
-          "#FFF59D",
-          "#DCE775",
-          "#8BC34A",
-          "#00796B",
-          "#006064"
-        ],
-        gutter: 10,
-        orientation: "vertical",
-        titleOrientation: "top",
-        style: {
-          data: {
-            type: "circle"
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          title: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 5,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
+          data: { strokeWidth: 1 },
+          labels: { fontSize: 8, fill: "#455A64" }
         }
       },
-      line: {
-        style: {
-          data: {
-            fill: "transparent",
-            opacity: 1,
-            stroke: "#455A64",
-            strokeWidth: 2
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 50
+      group: {
+        colorScale: ["#4a90e2", "#000099"]
       },
-      pie: {
-        colorScale: [
-          "#F4511E",
-          "#FFF59D",
-          "#DCE775",
-          "#8BC34A",
-          "#00796B",
-          "#006064"
-        ],
-        style: {
-          data: {
-            padding: 8,
-            stroke: "#ECEFF1",
-            strokeWidth: 1
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 20,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 50
-      },
-      scatter: {
-        style: {
-          data: {
-            fill: "#455A64",
-            opacity: 1,
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 8,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 50
-      },
-      stack: {
-        colorScale: [
-          "#F4511E",
-          "#FFF59D",
-          "#DCE775",
-          "#8BC34A",
-          "#00796B",
-          "#006064"
-        ],
-        width: 350,
-        height: 350,
-        padding: 50
-      },
-      tooltip: {
-        style: {
-          fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-          fontSize: 12,
-          letterSpacing: "normal",
-          padding: 5,
-          fill: "#455A64",
-          stroke: "transparent",
-          strokeWidth: 0,
-          pointerEvents: "none"
-        },
-        flyoutStyle: {
-          stroke: "#212121",
-          strokeWidth: 1,
-          fill: "#f0f0f0",
-          pointerEvents: "none"
-        },
-        cornerRadius: 5,
-        pointerLength: 10
-      },
-      voronoi: {
-        style: {
-          data: {
-            fill: "transparent",
-            stroke: "transparent",
-            strokeWidth: 0
-          },
-          labels: {
-            fontFamily: "'Roboto', 'Helvetica Neue', Helvetica, sans-serif",
-            fontSize: 12,
-            letterSpacing: "normal",
-            padding: 5,
-            fill: "#455A64",
-            stroke: "transparent",
-            strokeWidth: 0,
-            pointerEvents: "none"
-          },
-          flyout: {
-            stroke: "#212121",
-            strokeWidth: 1,
-            fill: "#f0f0f0",
-            pointerEvents: "none"
-          }
-        },
-        width: 350,
-        height: 350,
-        padding: 50
-      }
     };
-    
 
     return (
       <div className="App">
-        <VictoryChart theme={wincTheme}>
-          <VictoryGroup offset={2}>
+        <VictoryChart theme={wincTheme} height={350} width={800}>
+          <VictoryLabel 
+            text="Gemiddelde beoordeling van alle opdrachten" 
+            x={400} y={30} 
+            textAnchor="middle"
+            style={{ fill: '#4a90e2', fontSize: 18 }}  
+          />
+          <VictoryGroup offset={3}>
             <VictoryBar 
               data={avgRatingsDifficultyForGraph} 
-              labelComponent={<VictoryTooltip />}
+              labelComponent={
+                <VictoryTooltip 
+                  cornerRadius={5}
+                  pointerLength={10}
+                  flyoutStyle={{ stroke: "none", fill: "#4a90e2" }}
+                />
+              }
             />
             <VictoryBar 
               data={avgRatingsEnjoymentForGraph} 
-              labelComponent={<VictoryTooltip />}
+              labelComponent={
+                <VictoryTooltip 
+                  cornerRadius={5}
+                  pointerLength={10}
+                  flyoutStyle={{ stroke: "none", fill: "#4a90e2" }}
+                />
+              }
             />
           </VictoryGroup>
         </VictoryChart> 
